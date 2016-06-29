6a93f4b1ca4495bc4bb4323dc8817fda689243fc
Feilan Jiang <feilanjiang@gmail.com>
2016-06-29 16:42:27 -0400

    Add some db migration notes

--- src/backend/index.js
+++ src/backend/index.js
@@ -7,6 +7,15 @@
 // to run a test with auto db migrations:
 // ./node_modules/.bin/db-migrate up --env test && ./node_modules/.bin/jasmine-node --verbose --captureExceptions ./spec/
 
+/*db migration workflow:
+    1. changes in code reflect new db structure
+    2. create new migration file (1st command)
+    3. add db changes to migration file generated in previous step
+    4. remove old, manually created tables if needed ('rm' command)
+    5. run migration and update db (2nd command)
+    6. ready for testing
+*/
+
 'use strict';
 
 var Server = require('./server.js').Server; // contains 'Server()' function from server.js
